eea375eca54615927c9b451318b84954cbce6fb0
isra-figueroa <64873428+isra-figueroa@users.noreply.github.com>
2021-09-13 23:49:45 -0500

    Add files via upload

--- js/precio.js
+++ js/precio.js
@@ -21,6 +21,26 @@ $(document).ready(function() {
     var coleccion = db.ref().child("precios");
     var dataSet = [];//array para guardar los valores de los campos inputs del form
     var table = $('#tablaPrecio').DataTable({
+        language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
                 pageLength : 5,
                 lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']],
                 data: dataSet,
--- js/ventas.js
+++ js/ventas.js
@@ -22,6 +22,26 @@ $(document).ready(function() {
     var coleccion2 = db.ref().child("ventas");
     var dataSet = [];//array para guardar los valores de los campos inputs del form
     var table = $('#tablaVentas').DataTable({
+        language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
                 pageLength : 5,
                 lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']],
                 data: dataSet,
